fa99026953c4785f8b7b24f2ad5ecd73edaf72f9
Divya Yash <divyayashsaxena2000@gmail.com>
2022-11-20 11:36:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const dotenv = require('dotenv')
 const express = require('express')
 const app = express();
 const cors = require('cors')
@@ -11,10 +10,8 @@ app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 const User = require("./models/User");
-
-const Order = require("./models/Order");
-const Files = require("./models/Files");
-
+const ImeiRecord = require("./models/ImeiRecord");
+const Permissions = require("./models/Permissions");
 
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
@@ -25,121 +22,85 @@ app.use(express.json())
 
 mongoose.connect(process.env.DATABASE_URI)
 
-app.get('/api/read', (req,res) =>{
+app.get('/getRegisteredUsers', (req,res) =>{
 
-	Order.find({}).then((orders) =>{
-		res.send(orders);
+	User.find({}).then((users) =>{
+		res.status(200).send(users);
 	}).catch((err) =>{
 		res.status(500).send(err);
 	})
 })
 
-//
+app.get('/totalRegisteredUsers', (req,res) =>{
+	try{
+	User.countDocuments().then(async(count_documents) =>{
+		res.status(200).send({"count": count_documents});
+	})
+}
+catch(err){
+	res.status(404).send({message: error})
+}
+})
 
-//Order
+app.get('/totalUsers', (req,res) =>{
+	try{
+	ImeiRecord.countDocuments().then(async(count_documents) =>{
+		res.status(200).send({"count": count_documents});
+	})
+}
+catch(err){
+	res.status(404).send({message: error})
+}
+})
+
+app.get('/getPermissions', (req,res) =>{
+
+	Permissions.find({}).then((users) =>{
+		res.status(200).send(users);
+	}).catch((err) =>{
+		res.status(500).send(err);
+	})
+})
+
+app.get('/getImeiRecords', (req,res) =>{
+
+	ImeiRecord.find({}).then((users) =>{
+		res.status(200).send(users);
+	}).catch((err) =>{
+		res.status(500).send(err);
+	})
+})
+
 app.post('/api/remove', async (req, res) => {
 	const id = req.body.id;
 	try {
-		 Order.findByIdAndRemove(id).exec()
-		res.json({ status: 'ok', text:'order deleted'})
+		 User.findByIdAndRemove(id).exec()
+		res.json({ status: 'ok', text:'user deleted'})
 	} catch (err) {
 		res.json({ status: 'error', error: err })
 	}
 })
 
-
-app.delete('/api/delete', async (req, res) => {
+app.post('/remove/permissions', async (req, res) => {
 	const id = req.body.id;
-	await Order.findByIdAndRemove(id).exec()
-	res.send("Item Deleted")
-	// console.log(id)
-})
-
-
-app.put('/api/editsub', async(req, res) => {
-	const company=req.body.company
-	const name=req.body.name
-	const street=req.body.street
-	const postcode=req.body.postcode
-	const location =req.body.location
-	const email =req.body.email
-	const phone =req.body.phone
-	const vat =req.body.vat 
-
-
-	try{
-		const result = await User.updateOne({email: email},{
-			$set: {
-				company:company,
-				name:name,
-				street:street,
-				postcode:postcode,
-				location :location,
-				phone :phone,
-				vat :vat
-			}
-		});
-		// console.log(result);
-		res.json({ status: 'ok', text:'subcontractor info updated successfully'})
-	}
-	catch(err){
-		// console.log(err);
-		res.json({ status: 'error', error: err})
+	try {
+		 Permissions.findByIdAndRemove(id).exec()
+		res.json({ status: 'ok', text:'user deleted'})
+	} catch (err) {
+		res.json({ status: 'error', error: err })
 	}
 })
 
-app.get('/api/profiledata', (req,res) =>{
-	// console.log(req.query.email)
-	const email = req.query.email;
-
-
-	User.findOne({email:email}).then((subs) =>{
-// console.log(subs)
-		res.json({ status: 'ok', sub:subs})
-	}).catch((err) =>{
-		// console.log(err);
-		res.json({ status: 'error', error: err})
-	})
-})
-
-app.get('/api/filemail', async (req, res) => {
-	// console.log(req.query.email)
-	try{
-		const user = await Files.findOne({
-			email: req.query.email,
-		})
-		if(user){
-			res.json({ status: 'ok', text: 'Certificate exists' })
-		}
-		else{
-			res.json({ status: 'invalid', error: 'No certificate could be found' })
-		}
-
-	}
-	catch (err) {
-		res.json({ status: 'invalid', error: err })
+app.post('/remove/downloads', async (req, res) => {
+	const id = req.body.id;
+	try {
+		 ImeiRecord.findByIdAndRemove(id).exec()
+		res.json({ status: 'ok', text:'user deleted'})
+	} catch (err) {
+		res.json({ status: 'error', error: err })
 	}
 })
 
-app.post('/api/sendfile', async (req, res) => {
-
-	try{
-		if(req.body.file!==''){
-		await Files.create({
-			file: req.body.file,
-			email: req.body.email,
-		})
-		res.json({ status: 'ok' })
-	}
-	else{
-		res.json({ status: 'error', error: 'No file found' })
-	}
-	}
-	catch (err) {
-		res.json({ status: 'error', error: 'Upload Failed' })
-	}
-	
-})
 
 app.post('/api/register', async (req, res) => {
 	console.log(req.body.password)
@@ -163,8 +124,7 @@ app.post('/api/login', async (req, res) => {
 	})
 
 	if (!user) {
-		//For development env
-		// console.log("email not found bruhh")
+		
 		return { status: 'error', error: 'Invalid login', remarks:"Email address isn't valid" }
 	}
 	else{
@@ -194,34 +154,6 @@ app.post('/api/login', async (req, res) => {
 
 })
 
-//Order
-app.post('/api/order', async (req, res) => {
-	// console.log(req.body)
-	try {
-		await Order.create({
-			value: req.body.value,
-			images : req.body.images,
-			length : req.body.length,
-			sliderVal : req.body.sliderVal,
-			name : req.body.name,
-			email : req.body.email,
-			contact : req.body.contact,
-			address : req.body.address,
-			section : req.body.section,
-			others : req.body.others,
-			method: req.body.method,
-			floor: req.body.floor,
-        	date : req.body.date,
-			postcode : req.body.postcode,
-			city : req.body.city,
-			description : req.body.description,
-			orderstatus : req.body.orderstatus
-		})
-		res.json({ status: 'ok', text:'stored successfully'})
-	} catch (err) {
-		res.json({ status: 'error', error: err })
-	}
-})
 
 
 const PORT = process.env.PORT || 5000;
